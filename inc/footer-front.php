1f4bc31acfe1a802f2f384dfd90cb2c147cb60e2
Younes BESSA <bessa460@gmail.com>
2020-11-10 17:12:27 +0100

    rajout de la branche footer

--- inc/footer-front.php
+++ inc/footer-front.php
@@ -1,6 +1,8 @@
 
 
-  <footer>
+  <footer id="footer">
+  <span class="copyright">© HealthBook 2020. All rights reserved.</span>
+  <a href="./mentionlegal.php"><span class="mentionslegales">Mentions Légales</span></a>
   </footer>
 </body>
 </html>
