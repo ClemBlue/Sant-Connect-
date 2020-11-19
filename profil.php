232d9e5b88d4ab02e9ab1dc50ecd807d41d32867
Younes BESSA <bessa460@gmail.com>
2020-11-19 15:39:29 +0100

    modification et stylisation de la page profil

--- profil.php
+++ profil.php
@@ -96,15 +96,20 @@ if(!empty($_POST['submitvaccin'])){
 // Récup date des vaccins pour puis afficher temps restant //
 require('inc/header-front.php');
 ?>
-
-<p>Bienvenu <?php echo $nom . ' ' . $prenom; ?><p>
-
+<div class="bodyprofil">
+<div class="avatarname">
+  <img src="asset/img/avatar.png" alt="avatar" width="100">
+  <h4><?php echo $prenom . ' ' . $nom; ?></h4>
+</div>
+<div class="bienvenue">
+<p>Bienvenue, <?php echo $prenom . ' ' . $nom; ?><p>
+</div>
+<br>
 <?php
 $idTemp = -1;
 if (!empty($vaccinUser)) {
   foreach ($vaccinsOfUser as $vaccinOfUser) {
   $idTemp++;
-  echo $idTemp;
   echo $vaccinOfUser['name'];
   echo '</br>';
   echo $vaccinOfUser['description'];
@@ -124,6 +129,7 @@ echo '<p>Vous n\'avez ajouté aucun vaccin</p>';
 }
 ?>
 <!-- Ajout date de vaccin -->
+<div class="vaccinform">
 <form action="" method="post">
   <label for="name">Nom du Vaccin</label>
   <span class="error"><?php if(!empty($errors['name'])) { echo $errors['name']; } ?></span>
@@ -133,25 +139,25 @@ echo '<p>Vous n\'avez ajouté aucun vaccin</p>';
   <input type="date" name="date_vaccin" value="">
   <input type="submit" name="submitvaccin" value="Valider">
 </form>
-
+</div>
 <?php
 if (!empty($vaccinsAll)) {
   foreach ($vaccinsAll as $vaccinAll) {
     if(!empty($vaccinAll)){
+    echo '<div class="vaccin">';
     echo $vaccinAll['name'];
     echo '</br>';
     echo $vaccinAll['description'];
     echo '</br>';
-    echo '<a href="vaccins.php?id=' . $vaccinAll['id'] . '&&type=add">Ajouter à mes vaccins</a>';
-    echo '</br>';
+    echo '<a href="vaccins.php?id=' . $vaccinAll['id'] . '&&type=add">Ajouter à mes vaccins</a></br>';
+    echo '</div>';
     }
   }
 } else {
-  echo '<p>Plus de vaccins à ajouter</p>';
+  echo '<div> <p>Plus de vaccins à ajouter</p> </div>';
 }
 ?>
-
-<a href="logout.php">Déconexion</a>
-
+<a class="logout" href="logout.php">Déconnexion</a>
+</div>
 <?php
 require('inc/footer-front.php');
